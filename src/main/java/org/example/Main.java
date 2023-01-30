02e86bdcfa788e449ed8863e3dca1f8eeb4eba21
VIGNESH-TRL769 <123439677+VIGNESH-TRL769@users.noreply.github.com>
2023-01-30 18:12:52 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -53,7 +53,7 @@ public class Main
         LOGGER.info(newCaed);
     }catch(Exception e)
         {
-            String value = "Please Enter Valid Numbers:" + e;
+            String value = "Please Enter Validate Inputs:" + e;
             LOGGER.info(value);
             input.nextLine();
         }
